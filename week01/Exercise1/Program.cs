4d28c5e037d63170d8ea54b1aee577c1b3245c81
akellofatumakedrine-create <akellofatumakedrine@gmail.com>
2026-01-10 17:27:36 +0300

    week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name");
+        string firstName = Console.ReadLine();
+
+        //prompt for last name
+        Console.WriteLine("What is your last name");
+        string lastName = Console.ReadLine();
+
+        //print full name
+        Console.WriteLine("Your full name is " + firstName + " " + lastName
+        );
     }
 }
